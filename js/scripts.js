e2f9d12e030ebdaa378d0a4a886d134504815c3b
Asaph101 <chefasaph@gmail.com>
2018-04-08 21:20:18 +0300

    Update README.md file

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//Back-logic
+function rollDice(ansWer){
+    var thePigDiceArray=[];
+    for (var i=1,2,3,4,5,6; i=ansWer; i+=1,2,3,4,5,6);
+
+//Player-1
+
+$(document).ready(function(){
+    $("#user1#user-name").click (function(event){
+      event.preventDefault();
+      document.getElementById("#user1#user-name").style.display = "none";
+
+//Player-2
+
+$(document).ready(function(){
+    $("#user2#user-name").submit (function(event){
+      event.preventDefault();
+      document.getElementById("#user1#user-name").style.display = "none";
+      
+//User-interface
